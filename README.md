15c3f4cf128c806701546da95da11f2ac12b2920
Matthew Podwysocki <matthewp@microsoft.com>
2022-06-14 15:19:47 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Azure Tracing Sample
+
+This is a sample of using tracing with `@azure/core-tracing` to write to the console using a `ConsoleSpanExporter`.
+
+## LICENSE
+
+MIT
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
 const { createAzureSdkInstrumentation } = require("@azure/opentelemetry-instrumentation-azure-sdk");
-const { tracingClient } = require('./tracing');
 
-const opentelemetry = require("@opentelemetry/api");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { SimpleSpanProcessor, ConsoleSpanExporter } = require("@opentelemetry/tracing");
 
@@ -19,15 +17,10 @@ registerInstrumentations({
   instrumentations: [createAzureSdkInstrumentation()],
 });
 
-const tracer = opentelemetry.trace.getTracer("my-tracer");
-const span = tracer.startSpan("ot-main");
-const ctx = opentelemetry.trace.setSpan(opentelemetry.context.active(), span);
-const tracingOptions = {
-  tracingContext: ctx
-};
+const { tracingClient } = require('./tracing');
 
-async function main(tracingOptions) {
-  return tracingClient.withSpan("main", { tracingOptions: tracingOptions }, (updatedOptions) => {
+async function main() {
+  return tracingClient.withSpan("main", {}, (updatedOptions) => {
     logger.tracingClient = updatedOptions.tracingClient;
     logger.info("main!");
     return doWork(updatedOptions);
@@ -41,7 +34,6 @@ async function doWork(options) {
   });
 }
 
-main(tracingOptions)
+main()
   .then(() => console.log("done"))
-  .catch((err) => console.log(`Error ocurred ${err}`))
-  .finally(() => span.end());
+  .catch((err) => console.log(`Error ocurred ${err}`));
